38650fed75735ed7ca35e0252957c6f6214b5f1f
rstr74 <robbertstreng2012@gmail.com>
2015-06-02 22:56:47 +0200

    - Added some code comments and license reference - Test added

--- index.js
+++ index.js
@@ -41,9 +41,17 @@ module.exports = (function(scope) {
 		var exists = fs.existsSync(_path);
 
 		if (exists === true) {
+
+			// If exists, the file is located in the scope.basePath,
+			// or a temp path is given and it add up to an actual class file
+			
+			//console.log("_path:",_path);
+			//console.log("basePath:",scope.basePath);
 			if(scope.validateIncludePaths) scope.exists(_path);
 				return require(_path)(scope.include, scope, callback);
+
 		} else {
+			// Now it can be pointing to a class file in a module in npm 'node_modules' dir:
 			try {
 				var modulename = path.split("/")[0];
 				//console.log("modulename",modulename);
@@ -55,13 +63,22 @@ module.exports = (function(scope) {
 				//console.log("class_basedir",class_basedir);
 				var class_basedir_file = moduledirname + class_basedir + file + ".js";
 				//console.log("class_basedir_file",class_basedir_file);
+
+				// if class_basedir_file lacks a slash between class_basedir and the variable file, it create a invalid path, which is intended.
+				// This fails the exists test below and that points out it is a module, not an actual class file.
+				// class_basedir_file assignment above this comment, could produce for example:
+				// some_npm_module/SomeOtherComponentFromOtherModuleInRootDir.js which does not exists, so will skip
+				// to the else statement below this comment.
+				
 				var exists = fs.existsSync(class_basedir_file);
 				//console.log("exists",exists);
 				if (exists) {
-					if(scope.validateIncludePaths)
-						scope.exists(class_basedir_file);
+					// It points to an existing file in a path to a module. 
+					if(scope.validateIncludePaths) scope.exists(class_basedir_file);
 					return require(class_basedir_file)(scope.include, scope, callback);
 				} else {
+					// It does not exists, so it seem module name referece.
+					// Now try to resolve this module by the package.json "main" property
 					var resolvedPlugin = require.resolve(modulename);
 					var exists = fs.existsSync(resolvedPlugin);
 					if (exists) {
@@ -78,8 +95,36 @@ module.exports = (function(scope) {
 		}
 	}.bind(this);
 
+	
 	scope.exists = function(fullRequiredPath) { 
 
+	// inspired on the exist method of npm module valiquire
+	// https://github.com/thlorenz/valiquire/blob/master/validate-requires.js#L71
+	// 
+	// Copyright 2012 Thorsten Lorenz. 
+	// All rights reserved.
+
+	// Permission is hereby granted, free of charge, to any person
+	// obtaining a copy of this software and associated documentation
+	// files (the "Software"), to deal in the Software without
+	// restriction, including without limitation the rights to use,
+	// copy, modify, merge, publish, distribute, sublicense, and/or sell
+	// copies of the Software, and to permit persons to whom the
+	// Software is furnished to do so, subject to the following
+	// conditions:
+
+	// The above copyright notice and this permission notice shall be
+	// included in all copies or substantial portions of the Software.
+
+	// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+	// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+	// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+	// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+	// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+	// OTHER DEALINGS IN THE SOFTWARE.
+	
     var warning = [];
     // Check directory
     // cwd() gives us the path in correct casing, no function of fs/path module does that 
---
+++ test/validateIncludePathsSlashTest.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+var should = require("should");
+var debug = require('debug')('test');
+
+var puremvc = require("../index.js");
+
+puremvc.validateIncludePaths = true;
+
+// TO-DO
